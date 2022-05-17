39d1b6d030dadddc24b6658d987f010f8209c001
And4Web <anands2807@gmail.com>
2022-05-17 07:17:10 +0530

    signup page and chart page created

---
+++ src/pages/chart-page/ChartPageComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChartPageComponent() {
+  return <div>ChartPageComponent</div>;
+}
+
+export default ChartPageComponent;
---
+++ src/pages/signup-page/SignupPageComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./signupPageComponent.scss";
+
+function SignupPageComponent() {
+  return <div>Signup-page-component</div>;
+}
+
+export default SignupPageComponent;
